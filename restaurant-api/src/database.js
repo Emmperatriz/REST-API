3187384b70aba7b0cbdb6e1964cb526696023976
emperatriz <empe_ambriz@hotmail.com>
2021-02-01 09:25:08 -0600

    Agregue conexion a Heroku

--- restaurant-api/src/database.js
+++ restaurant-api/src/database.js
@@ -1,14 +1,16 @@
 //DATABASE CONECTION
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+/* const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '16061995eab17',
     database: 'Melp'
 
 
-});
+}); */
+
+const mysqlConnection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
 mysqlConnection.connect(function (err) {
     if (err) {
